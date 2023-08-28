6dd0d7aa1f47ca2ce1e061c24506a6c518b35254
Kvete <113244503+Kvete@users.noreply.github.com>
2023-08-28 14:21:23 +0300

    Add files via upload

---
+++ words_recursion.py
@@ -0,0 +1,17 @@
+from random import randint
+
+
+def recursion(alphabet, word, length=0):
+    if length == len(word):
+        print(word)
+        return
+    for i in range(len(alphabet)):
+        word[length] = alphabet[i]
+        recursion(alphabet, word, length + 1)
+
+
+t = randint(1, 7)  # numer of letters in word
+
+alphabets = ['a', 'b', 'c', 'd']
+words = [0 for j in range(t)]
+recursion(alphabets, words)
